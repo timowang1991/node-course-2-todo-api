3bce54066b9680240bcf64f92a464ae1dc6b05bd
Timo Wang <chiuanw@yahoo-inc.com>
2017-07-29 16:53:53 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const { ObjectID } = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +29,32 @@ app.get('/todos', (req, res) => {
         });
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// GET /todos/:todoid
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // Valid id using isValid
+    // if not valid will be 404 - send back empty body
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    // find by id
+    // success
+        // if todo -send it back
+        // if no todo - send back 404 with empty body
+    // error
+        // 400 - and send empty body back
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
