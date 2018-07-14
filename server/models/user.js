758e508c60582cc50619e7523041b5182276f121
Timo Wang <chiuanw@yahoo-inc.com>
2018-07-14 22:24:37 +0800

    Add Get /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -46,9 +46,30 @@ UserSchema.methods.generateAuthToken = function () {
     var access = 'auth';
     var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
 
-    user.tokens.push({ access, token });
+    // user.tokens.push({ access, token });
+    user.tokens = user.tokens.concat([{ access, token }]);
 
     return user.save().then(() => token);
+};
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+    
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // });
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
 };
 
 var User = mongoose.model('User', UserSchema);
